07af81d61a17a67bf484f6a8e87e1d418e7e9c1c
darshan744 <darshankumaravelk@gmail.com>
2025-05-21 20:47:17 +0530

    feat : Installed mason plugin manager setup language server protocol todo : auto completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,23 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function ()
-			vim.lsp.enable("angularls")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+			local lspconfig = require("lspconfig")
+			
+			-- Add your LSP setups below
+			lspconfig.angularls.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-
-		end
-
-	}
-
+    end,
+  }
 }
